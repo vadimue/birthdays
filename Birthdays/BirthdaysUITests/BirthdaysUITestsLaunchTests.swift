bf4d983ca25fb01e061dcf0dc2507a7ad9e36cbd
Vadym Brusko <vaadim.br@gmail.com>
2022-07-15 13:21:34 +0300

    Create project and add packages
    - Add Apollo for GraphQL
    - Add SnapKit to build UI from code quickly

---
+++ Birthdays/BirthdaysUITests/BirthdaysUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BirthdaysUITestsLaunchTests.swift
+//  BirthdaysUITests
+//
+//  Created by Vadym Brusko on 15.07.2022.
+//
+
+import XCTest
+
+class BirthdaysUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
